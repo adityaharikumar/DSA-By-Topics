b46c98bc3f6882caf6659746445d54281c9e98cd
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-10 20:24:19 +0530

    Time: 15 ms (64.49%), Space: 42.4 MB (78.33%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        boolean[] dp = new boolean[n + 1];
-        for(int i=1;i<=n;i++){
+        boolean[] dp = new boolean[n+1];
+        for(int i=0;i<=n;i++){
             for(int j=1;j*j<=i;j++){
-                if(!dp[i-j*j]){
+                int remian = i-j*j;
+                if(!dp[remian]){
                     dp[i]=true;
                     break;
                 }
