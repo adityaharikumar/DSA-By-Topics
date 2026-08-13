ab5e26d62d71b59a90ad9ae48fcf4c71d1f29479
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-13 17:44:41 +0530

    Time: 110 ms (45.31%), Space: 158.2 MB (10.94%) - LeetHub

---
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
@@ -0,0 +1,118 @@
+class Solution {
+
+    class Node {
+        char leftChar;
+        char rightChar;
+
+        int prefix;
+        int suffix;
+        int best;
+        int len;
+
+        Node() {}
+
+        Node(char ch) {
+            leftChar = ch;
+            rightChar = ch;
+            prefix = 1;
+            suffix = 1;
+            best = 1;
+            len = 1;
+        }
+    }
+
+    Node[] tree;
+    char[] arr;
+
+    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+
+        arr = s.toCharArray();
+
+        int n = arr.length;
+        tree = new Node[4 * n];
+
+        build(1, 0, n - 1);
+
+        int[] ans = new int[queryIndices.length];
+
+        for (int i = 0; i < queryIndices.length; i++) {
+
+            int index = queryIndices[i];
+            char ch = queryCharacters.charAt(i);
+
+            arr[index] = ch;
+
+            update(1, 0, n - 1, index, ch);
+
+            ans[i] = tree[1].best;
+        }
+
+        return ans;
+    }
+
+    private void build(int node, int left, int right) {
+
+        if (left == right) {
+            tree[node] = new Node(arr[left]);
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        build(node * 2, left, mid);
+        build(node * 2 + 1, mid + 1, right);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private void update(int node, int left, int right, int index, char ch) {
+
+        if (left == right) {
+            tree[node] = new Node(ch);
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        if (index <= mid) {
+            update(node * 2, left, mid, index, ch);
+        } else {
+            update(node * 2 + 1, mid + 1, right, index, ch);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private Node merge(Node a, Node b) {
+
+        Node res = new Node();
+
+        res.len = a.len + b.len;
+
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+        // Prefix
+        res.prefix = a.prefix;
+
+        if (a.prefix == a.len && a.rightChar == b.leftChar) {
+            res.prefix = a.len + b.prefix;
+        }
+
+        // Suffix
+        res.suffix = b.suffix;
+
+        if (b.suffix == b.len && a.rightChar == b.leftChar) {
+            res.suffix = b.len + a.suffix;
+        }
+
+        // Best
+        res.best = Math.max(a.best, b.best);
+
+        if (a.rightChar == b.leftChar) {
+            res.best = Math.max(res.best, a.suffix + b.prefix);
+        }
+
+        return res;
+    }
+}
