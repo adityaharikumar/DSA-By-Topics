bb5345949221de4807d30df31eaf6067dc5deb83
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-14 19:29:47 +0530

    Time: 54 ms (58.5%), Space: 99.6 MB (10.3%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,38 +1,25 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-
-        HashSet<Integer> set = new HashSet<>();
-
-        long sum = 0;
-        long maxSum = 0;
-
+        HashMap<Integer,Integer> map = new HashMap<>();
         int left = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-
-            // If duplicate exists, remove from left
-            while (set.contains(nums[right])) {
-                set.remove(nums[left]);
-                sum -= nums[left];
+        long max = 0;
+        long sum=0;
+        for(int right=0;right<nums.length;right++){
+             sum+=nums[right];
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            if(right-left+1>k){
+                sum-=nums[left];
+                map.put(nums[left],map.get(nums[left])-1);
+                if(map.get(nums[left])==0){
+                    map.remove(nums[left]);
+                    
+                }
                 left++;
             }
-
-            // Add current element
-            set.add(nums[right]);
-            sum += nums[right];
-
-            // If window size becomes k
-            if (right - left + 1 == k) {
-
-                maxSum = Math.max(maxSum, sum);
-
-                // Remove left for next window
-                set.remove(nums[left]);
-                sum -= nums[left];
-                left++;
+            if(right-left+1==k && map.size()==k){
+                max = Math.max(max,sum);
             }
         }
-
-        return maxSum;
+        return max;
     }
 }
