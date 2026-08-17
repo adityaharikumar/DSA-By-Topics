340e2a6c09598dd5c996823701576ae919ca5b8f
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-17 21:29:04 +0530

    Time: 32 ms (63.04%), Space: 184.4 MB (53.8%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.java
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.java
@@ -3,6 +3,7 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n-k+1];
         int[] freq = new int[51];
+
         for(int i=0;i<k;i++){
             if(nums[i]<0){
                 freq[-nums[i]]++;
@@ -18,12 +19,14 @@ class Solution {
                 freq[-nums[right]]++;
             }
             ans[right-k+1]=find(freq,x);
+
         }
         return ans;
+
     }
     private int find(int[] freq,int x){
         int count=0;
-        for(int i=50;i>=1;i--){
+        for(int i=50;i>=0;i--){
             count+=freq[i];
             if(count>=x){
                 return -i;
