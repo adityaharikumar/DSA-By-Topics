9cccae898dc68d487c391df4dca4330521d1c1b1
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-20 23:38:05 +0530

    Time: 23 ms (86.43%), Space: 81 MB (88.53%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+       int n = nums.length;
+       int[] dp = new int[n];
+       Deque<Integer> deque = new ArrayDeque<>();
+       dp[0]=nums[0];
+       deque.offerLast(0);
+       for(int i=1;i<n;i++){
+        while(!deque.isEmpty() && deque.peekFirst()<i-k){
+            deque.pollFirst();
+        }
+        dp[i]=nums[i]+dp[deque.peekFirst()];
+        while(!deque.isEmpty() && dp[deque.peekLast()]<=dp[i]){
+            deque.pollLast();
+        }
+        deque.offerLast(i);
+       } 
+       return dp[n-1];
+    }
+}
