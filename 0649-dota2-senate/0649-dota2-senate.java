5f9d506a45fd87c82339e8e7106b2445a56b7827
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-22 01:55:23 +0530

    Time: 12 ms (44.89%), Space: 47.4 MB (20.74%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,6 +1,6 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-      Queue<Integer> rad = new LinkedList<>();
+     /* Queue<Integer> rad = new LinkedList<>();
       Queue<Integer> di = new LinkedList<>();
 
       int n = senate.length();
@@ -24,7 +24,31 @@ class Solution {
         return "Radiant";
       }else{
         return "Dire";
-      }
+      }*/
 
+    int n = senate.length();
+    Queue<Integer> rad = new LinkedList<>();
+    Queue<Integer> di = new LinkedList<>();
+    for(int i=0;i<n;i++){
+        if(senate.charAt(i)=='R'){
+            rad.add(i);
+        }else{
+            di.add(i);
+        }
+    }
+    while(!rad.isEmpty() && !di.isEmpty()){
+        int r = rad.poll();
+        int d = di.poll();
+        if(r<d){
+            rad.add(n+r);
+        }else{
+            di.add(n+d);
+        }
+    }
+    if(!rad.isEmpty()){
+        return "Radiant";
+    }else{
+        return "Dire";
+    }
     }
 }
