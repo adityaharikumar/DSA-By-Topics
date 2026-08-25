1649efde124d7e321ce4997e2bbc264c87956fe6
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-25 20:58:14 +0530

    Time: 25 ms (52.06%), Space: 81.8 MB (74.6%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,33 +1,21 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-        
+
         int n = nums.length;
         int[] dp = new int[n];
-
+        dp[0]=nums[0];
         Deque<Integer> deque = new LinkedList<>();
-
-        dp[0] = nums[0];
         deque.offer(0);
-
-        for (int i = 1; i < n; i++) {
-
-            // Remove indices outside the jump range
-            while (!deque.isEmpty() && deque.peekFirst() < i - k) {
+        for(int i =1;i<n;i++){
+            while(!deque.isEmpty() && deque.peekFirst()<i-k){
                 deque.pollFirst();
             }
-
-            // Use the maximum dp value
-            dp[i] = nums[i] + dp[deque.peekFirst()];
-
-            // Remove smaller dp values from the back
-            while (!deque.isEmpty() &&
-                   dp[deque.peekLast()] <= dp[i]) {
+            dp[i]=nums[i]+dp[deque.peekFirst()];
+            while(!deque.isEmpty() && dp[deque.peekLast()]<=dp[i]){
                 deque.pollLast();
             }
-
             deque.offerLast(i);
         }
-
-        return dp[n - 1];
+        return dp[n-1];
     }
 }
