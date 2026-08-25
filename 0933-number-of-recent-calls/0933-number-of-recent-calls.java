a057f4366e8fb2bccdd58c1e99538f6b8e45a17e
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-25 19:48:36 +0530

    Time: 22 ms (52.12%), Space: 59.5 MB (61.71%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,13 +1,18 @@
 class RecentCounter {
-    Queue<Integer> queue;
+    Queue<Integer> queue ;
     public RecentCounter() {
-        queue=new LinkedList<>();
+        queue = new LinkedList<>();
+
     }
     
     public int ping(int t) {
         queue.add(t);
-        while(queue.peek()<t-3000){
-            queue.poll();
+        while(!queue.isEmpty()){
+            if(queue.peek()<t-3000){
+                queue.poll();
+            }else{
+                break;
+            }
         }
         return queue.size();
     }
