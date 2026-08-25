100cbe336ace685ee8e9e6511d5c2c172f79fb72
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-25 20:08:28 +0530

    Time: 23 ms (48.63%), Space: 47.9 MB (88.42%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,55 +1,32 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-
-        // 1. Count frequency of each task
         int[] freq = new int[26];
-
-        for (char task : tasks) {
-            freq[task - 'A']++;
+        for(char task:tasks){
+            freq[task-'A']++;
         }
-
-        // 2. Max heap
-        PriorityQueue<Integer> pq =
-            new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int f : freq) {
-            if (f > 0) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int f:freq){
+            if(f>0){
                 pq.offer(f);
             }
         }
-
-        // 3. Queue: [remainingCount, availableTime]
         Queue<int[]> queue = new LinkedList<>();
-
         int time = 0;
-
-        // Continue while tasks remain
-        while (!pq.isEmpty() || !queue.isEmpty()) {
-
+        while(!pq.isEmpty()|| !queue.isEmpty()){
             time++;
-
-            // Move cooled-down tasks back to heap
-            while (!queue.isEmpty() && queue.peek()[1] <= time) {
+            while(!queue.isEmpty() && queue.peek()[1]==time){
                 pq.offer(queue.poll()[0]);
             }
-
-            // Execute a task
-            if (!pq.isEmpty()) {
-
+            if(!pq.isEmpty()){
                 int count = pq.poll();
-
                 count--;
-
-                // Task still has remaining copies
-                if (count > 0) {
-                    queue.offer(new int[] {
-                        count,
-                        time + n + 1
+                if(count>0){
+                    queue.offer(new int[]{
+                        count,time+n+1
                     });
                 }
             }
         }
-
         return time;
     }
 }
