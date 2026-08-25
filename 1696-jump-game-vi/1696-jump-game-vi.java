f18e967f854c99442067d293cb82b836a7382cca
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-25 20:47:58 +0530

    Time: 25 ms (52.06%), Space: 81.9 MB (72.06%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,36 +1,33 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-       /*int n = nums.length;
-       int[] dp = new int[n];
-       Deque<Integer> deque = new ArrayDeque<>();
-       dp[0]=nums[0];
-       deque.offerLast(0);
-       for(int i=1;i<n;i++){
-        while(!deque.isEmpty() && deque.peekFirst()<i-k){
-            deque.pollFirst();
+        
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        Deque<Integer> deque = new LinkedList<>();
+
+        dp[0] = nums[0];
+        deque.offer(0);
+
+        for (int i = 1; i < n; i++) {
+
+            // Remove indices outside the jump range
+            while (!deque.isEmpty() && deque.peekFirst() < i - k) {
+                deque.pollFirst();
+            }
+
+            // Use the maximum dp value
+            dp[i] = nums[i] + dp[deque.peekFirst()];
+
+            // Remove smaller dp values from the back
+            while (!deque.isEmpty() &&
+                   dp[deque.peekLast()] <= dp[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
         }
-        dp[i]=nums[i]+dp[deque.peekFirst()];
-        while(!deque.isEmpty() && dp[deque.peekLast()]<=dp[i]){
-            deque.pollLast();
-        }
-        deque.offerLast(i);
-       } 
-       return dp[n-1];
-    }*/
-    int n = nums.length;
-    int[] dp = new int[n];
-    Deque<Integer> deque = new ArrayDeque<>();
-    dp[0]=nums[0];
-    deque.offerLast(0);
-    for(int i=1;i<nums.length;i++){
-        while(!deque.isEmpty() && deque.peekFirst()<i-k){
-            deque.pollFirst();
-        }
-        dp[i]=nums[i]+dp[deque.peekFirst()];
-        while(!deque.isEmpty() && dp[deque.peekLast()]<=dp[i]){
-            deque.pollLast();
-        }
-        deque.offerLast(i);
+
+        return dp[n - 1];
     }
-    return dp[n-1];
-}}
+}
