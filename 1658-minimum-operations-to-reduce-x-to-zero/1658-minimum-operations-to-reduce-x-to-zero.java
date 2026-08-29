fe4a709214d75dd4d1734061a6b766a6df5d6331
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-08-29 06:34:44 +0530

    Time: 4 ms (98.16%), Space: 102 MB (59.71%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int count=0;
+        int count =0;
         int sum=0;
         for(int num:nums){
             sum+=num;
         }
-        int target=sum-x;
+        int target = sum-x;
         int left=0;
-        int right=0;
         int currentSum=0;
         int maxLen=-1;
-        while(right<nums.length){
+        for(int right=0;right<nums.length;right++){
             currentSum+=nums[right];
             while(currentSum>target && left<=right){
                 currentSum-=nums[left];
@@ -19,9 +18,7 @@ class Solution {
             if(currentSum==target){
                 maxLen=Math.max(maxLen,right-left+1);
             }
-            right++;
         }
         return maxLen==-1?-1:nums.length-maxLen;
-
     }
 }
