9ae9261d521e1ead1d6419a9aa52e55caacfaf1e
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-09-05 08:52:09 +0530

    Time: 8 ms (99.64%), Space: 59.5 MB (48.57%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,40 +10,40 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode slow = head;
-        ListNode fast = head.nextl
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        ListNode right = slow.next;
-        slow.next=null;
-        ListNode left = sortList(head);
-        right = sortList(right);
-        return merge(left,right);
+       if(head==null || head.next==null){
+        return head;
+       } 
+       ListNode slow = head;
+       ListNode fast = head;
+       while(fast.next!=null && fast.next.next!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+       }
+       ListNode second = slow.next;
+       slow.next=null;
+       ListNode left = sortList(head);
+       ListNode right = sortList(second);
+       return merge(left,right);
     }
-    private ListNode merge(ListNode left,ListNode right){
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        while(left!=null && right!=null){
-            if(left.val<=right.val){
-                curr.next = left;
-                left=left.next;
-            }else{
-                curr.next = right;
-                right = right.next;
-            }
-            curr=curr.next;
+   private ListNode merge(ListNode left,ListNode right){
+    ListNode dummy = new ListNode(0);
+    ListNode curr = dummy;
+    while(left!=null && right!=null){
+        if(left.val<=right.val){
+            curr.next = left;
+            left=left.next;
+        }else{
+            curr.next = right;
+            right = right.next;
         }
-        if(left!=null){
-            curr.next=left;
-        }
-        if(right!=null){
-            curr.next=right;
-        }
-        return dummy.next;
+        curr=curr.next;
     }
+     if(left!=null){
+        curr.next=left;
+     }
+     if(right!=null){
+        curr.next = right;
+     }
+     return dummy.next;
+   }
 }
