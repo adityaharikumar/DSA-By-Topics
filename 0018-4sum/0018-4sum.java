99af529bddbe018d3a5d2a39d52d986cc1d3d69f
ADITYA HARIKUMAR <168883526+adityaharikumar@users.noreply.github.com>
2026-09-25 20:41:39 +0530

    Time: 19 ms (81.23%), Space: 45.8 MB (63.06%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,27 +6,26 @@ class Solution {
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            for(int j=i+1;j<nums.length-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]){
-                    continue;
-                }
-            
+        for(int j=i+1;j<nums.length-2;j++){
+            if(j>i+1 && nums[j]==nums[j-1]){
+                continue;
+            }
             int left = j+1;
             int right = nums.length-1;
+
             while(left<right){
-                long sum = (long)nums[i]+nums[left]+nums[right]+nums[j];
+                long sum = (long) nums[i]+nums[j]+nums[left]+nums[right];
+
                 if(sum==target){
                     result.add(Arrays.asList(
-                       nums[i],
-                        nums[j],
-                        nums[left],
-                        nums[right]
+                        nums[i],nums[j],nums[left],nums[right]
                     ));
                     left++;
                     right--;
                     while(left<right && nums[left]==nums[left-1]){
                         left++;
-                    }while(left<right && nums[right]==nums[right+1]){
+                    }
+                    while(left<right && nums[right]==nums[right+1]){
                         right--;
                     }
                 }else if(sum<target){
@@ -35,7 +34,8 @@ class Solution {
                     right--;
                 }
             }
-        }}
+        }
+        }
         return result;
     }
 }
